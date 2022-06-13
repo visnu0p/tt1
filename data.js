94be4f5701fa4e3da79dbfdd8661a3fb7a4fe5c0
visnu0p <104520802+visnu0p@users.noreply.github.com>
2022-06-13 21:48:17 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,29 @@
+module.exports = {
+
+    mongoLink:'mongodb+srv://clickbot1:water786@cluster0.j9c1z.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',//database link
+    
+    bot_token:'5102616920:AAGGmGLvlQoffk26r-8yh3oN6lvBoStHOi4',//bot token
+    
+    bot_name:'Jdudueysbot',//bot username without @
+    
+    bot_admin:65555,// admin telegram id
+    
+    channelsList : ['@clickgonews'],// channels list
+    
+    reffer_bonus:100,// refferal bonus amount
+    
+    min_wd:0.2,//min withdrawal amount
+    
+    daily_bonus:100,// daily bonus amount
+    
+    currency:'net celo', //bot cuurency
+    
+    cb_api_key:'',//coinbase api key
+    
+    cb_api_secret:'',// coinbase secret key
+    
+    cb_account_id:'',// counbase account id for the bot currency
+    
+    payment_channel:'@clickgonews'// payment channel username
+    
+    }
